06b119d9906a878de5d96d17467d34c465a49991
AndreaMCH24 <99840321+AndreaMCH24@users.noreply.github.com>
2022-05-12 12:52:34 -0600

    Interfazez1

---
+++ src/interfacesmultiples/Edificio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesmultiples;
+
+/**
+ *
+ * @author Juana García
+ */
+public interface Edificio {
+    double getSuperficieEdificio();
+   
+    @Override
+    String toString();
+}
